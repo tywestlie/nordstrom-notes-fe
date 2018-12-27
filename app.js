b24312cf9b366f61cfb5790fd85c0b8410bf6a6c
Tyler Westlie <tyler.westlie@gmail.com>
2018-12-27 12:20:12 -0700

    makes request to notes index and displays on the page

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-document.getElementbyId('getText').addEventlistener('click', getText);
-
-function getText(){
-  alert("Hello!")
+$(document).ready(function(){
+  $('.dropdown-trigger').dropdown();
+   $('select').formSelect();
+ });
+function getNotes(){
+  fetch('https://e11gjajxwe.execute-api.us-east-1.amazonaws.com/dev/notes')
+  .then((res) => res.json())
+  .then((data) => {
+    let output = '<h2>Notes</h2>';
+    let notes = data.notes
+    notes.forEach(function(note){
+      output += `
+      <div class="divider"></div>
+        <div class="section">
+        <h5>${note.tag}</h5>
+        <p>${note.message}</p>
+        <p>Date Published:${note.date}</p>
+        </div>
+      `
+    });
+    document.getElementById('output').innerHTML = output;
+  })
 }
+
+getNotes();
